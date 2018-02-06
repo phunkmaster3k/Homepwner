f0dd308ea87e1d40080b9b82f445d3618d617e70
Nathan <nathan@nathans-mac-pro.fios-router.home>
2018-02-06 09:52:29 -0800

    working on gold challenge

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -10,17 +10,30 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    var splitItems = [[Item](), [Item]()]
     
     init() {
-        for _ in 0..<5 {
+        for _ in 0..<10 {
             createItem()
         }
     }
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
+        
         allItems.append(newItem)
+        
+        //adding here as to not break original functionality too much
+        if newItem.valueInDollars > 50 {
+            splitItems[0].append(newItem)
+        } else {
+            splitItems[1].append(newItem)
+            
+        }
+    
         return newItem
     }
+    
+    
     
 }
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     
+
     var itemStore: ItemStore!
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return itemStore.splitItems.count
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return itemStore.splitItems[section].count + 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "More than $50"
+        } else if section == 1 {
+            return "Less than $50"
+        } else {
+            return nil
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -21,12 +36,30 @@ class ItemsViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
-        let item = itemStore.allItems[indexPath.row]
+        //let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        if indexPath.row < itemStore.splitItems[indexPath.section].count {
+            let item = itemStore.splitItems[indexPath.section][indexPath.row]
+            
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            
+        } else {
+            cell.textLabel?.text = "No more items"
+            cell.detailTextLabel?.text = ""
+        }
+       
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row < itemStore.splitItems[indexPath.section].count {
+            return 60
+        }
+        return tableView.rowHeight
+    }
+    
+    override func tab
     
     override func viewDidLoad() {
         super.viewDidLoad()
